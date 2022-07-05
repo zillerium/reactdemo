9f8612ab14fb83a9101dd69d196aa74a0df8a46b
zillerium <sales@merrows.co.uk>
2022-07-05 08:38:38 +0100

    props1

--- props/src/components/ExpenseItem.js
+++ props/src/components/ExpenseItem.js
@@ -1,21 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 // props can be named anything, this has kv pairs
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+  
 
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const year = props.date.getFullYear();
+
 
   return (<div className="expense-item">
-             <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-             </div>
+             <ExpenseDate date={props.date}/>
+       
              <div>{props.date.toISOString()}</div>
              <div className="expense-item__description"> <h2>{props.title}</h2></div>
              <div className="expense-item__price">{props.amount}</div>
