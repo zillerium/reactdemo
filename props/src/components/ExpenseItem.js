0c037bb67dae7f1465f23dba3d3520d8d0eac7ac
zillerium <sales@merrows.co.uk>
2022-07-05 08:51:43 +0100

    v1 css

--- props/src/components/ExpenseItem.js
+++ props/src/components/ExpenseItem.js
@@ -10,7 +10,7 @@ function ExpenseItem(props) {
   return (<div className="expense-item">
              <ExpenseDate date={props.date}/>
        
-             <div>{props.date.toISOString()}</div>
+         
              <div className="expense-item__description"> <h2>{props.title}</h2></div>
              <div className="expense-item__price">{props.amount}</div>
           </div>)
