771d23bd87a7d5a4f5bdf1cb57cb4c9ffd08a62e
zillerium <sales@merrows.co.uk>
2022-07-10 06:18:55 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Hello my name is John
         </p>
         <a
           className="App-link"
