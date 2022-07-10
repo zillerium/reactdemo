3a6b6eda90a07c3c5127e850c436f903dd5562ad
zillerium <sales@merrows.co.uk>
2022-07-10 07:18:46 +0100

    demo1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+ 
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello my name is John
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'trevor oakley'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello {this.state.name}
+          </p>
+          <button onClick={() => {
+            this.state.name = 'john';
+            console.log(this.state);
+          }}>chg name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
