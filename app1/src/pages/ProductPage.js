26252a64defbd44e7ea87ebe0751da8530a036b8
sales@merrows.co.uk <sales@merrows.co.uk>
2023-01-01 04:54:28 +0000

    params

--- app1/src/pages/ProductPage.js
+++ app1/src/pages/ProductPage.js
@@ -1,12 +1,23 @@
-import {useParams} from "react-router-dom";
+import {useParams, useLocation, useNavigate} from "react-router-dom";
+//import React, {useState} from 'react';
 
 const ProductPage = (props) => {
-  return (
+	const {productId}=useParams();
+	const myloc = useLocation().state;
+//	const [location, setLocation]=useState();
+//	console.log("state = "+JSON.stringify(location));
+//	console.log("use params = "+JSON.stringify(useParams));
+	console.log("productId = "+productId);
+	console.log("myloc = "+JSON.stringify(myloc));
+	console.log("props = "+JSON.stringify(props));
+	return (
 	  <div>
-	  <h1>Product {props.productId}</h1>
+	  <h1>Product {productId}</h1>
 	  <p>Product is {props.desc}</p>
 	  </div>
   )
 }
 
 export default ProductPage;
+
+//export default ({match: {params:{id}}}) =>  <h1>{id}</h1>;
