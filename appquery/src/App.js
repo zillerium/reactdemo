c99b0b517ce88b1e8e0226d2383d3c601b7125f1
sales@merrows.co.uk <sales@merrows.co.uk>
2023-01-04 10:59:10 +0000

    usemutation

--- appquery/src/App.js
+++ appquery/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
-import {useQuery} from 'react-query';
+import {useQuery, useMutation} from 'react-query';
 import axios from 'axios';
 
 
@@ -9,6 +9,8 @@ function App() {
 
 	const [image, setImage]=useState(false);
 
+	const [firstName, setFirstName]=useState("");
+	const [lastName, setLastName]=useState("");
 
 	const {isLoading, error, data, isFetching, refetch} = useQuery('dogs',
 		() => axios ('https://random.dog/woof.json'),
@@ -22,7 +24,32 @@ const Button = () => {
 return	  <button onClick={refetch}>Get </button>
 }
 
+const PostData = async  (user) => {
+	let x = {keyword: user.firstName};
+	const response = await axios.post("https://peacioapi.com:3000/getDBData", x);
+//	console.log(x);
+//	const response = await fetch("https://peacioapi.com:3000/getDBData", {
+  //         method: 'POST',
+//	   body: x,
+//	headers: {
+  //         'Content-type': 'application/json; charset-UTF-8'
+//	}
+//	})
+	return response;
 
+
+}
+
+const {mutate,  isError} = useMutation(PostData, {
+	onSuccess: (successData) => {
+		console.log("post was done");
+
+           console.log(successData);
+	}
+})
+const ImageDisplay = () => {
+return ( data ? <img src={data.data.url}/> : <p></p>);
+}
  console.log("render");
  console.log(error);
  console.log(data);
@@ -39,7 +66,18 @@ return	  <button onClick={refetch}>Get </button>
 	  <button onClick={refetch}>Get </button>
          </div>
 	  Learn React
-	  <img src={data.data.url}/>
+	  <div>
+            <label>First Name </label>{firstName}
+	    <input type="text" placeholder="First " value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
+	  </div>
+	  <div>
+            <label>Last Name </label>
+	    <input type="text" placeholder="Last " value={lastName} onChange={(e)=>setLastName(e.target.value)} />
+	  </div>
+	  <div>
+           <button onClick={()=>mutate({firstName:firstName, lastName, lastName})}>Post</button>
+	  </div>
+	  <ImageDisplay />
       </header>
     </div>
   );
