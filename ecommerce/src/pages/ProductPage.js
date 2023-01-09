4e0a4a13d835cf8ee48c0c53a735c502ef129168
sales@merrows.co.uk <sales@merrows.co.uk>
2023-01-09 16:21:00 +0000

    ecommerce

--- ecommerce/src/pages/ProductPage.js
+++ ecommerce/src/pages/ProductPage.js
@@ -1,6 +1,8 @@
 import {useParams, useLocation, useNavigate} from "react-router-dom";
 import React, {useState, useContext} from 'react';
 import axios from 'axios';
+import {useQuery}  from 'react-query';
+import {Container, Button, Row, Col, Image} from 'react-bootstrap';
 
 import {CartContext} from '../CartContext';
 
@@ -12,7 +14,20 @@ import {CartContext} from '../CartContext';
 const ProductPage = (props) => {
 const [search, setSearch] = useState("");
 	const {productId}=useParams();
-      const cart=useContext(CartContext);	
+      const cart=useContext(CartContext);
+      const baseUrl = "https://peacioapi.com:3000/getPart/"+productId;
+
+console.log(baseUrl);
+      const {data, isLoading, isError, refetch}= useQuery(["cat"],() => {
+               return   axios.get(baseUrl).then((res)=>res.data);
+      });
+
+if (isError) {
+    return <h1>error</h1>
+}
+
+	if (isLoading) return <h1>Loading ..</h1>
+console.log(data.data[0]);
 	  const searchDB = async (searchVal) => {
 
           const baseUrl = "https://peacioapi.com:3000/getPart/"+searchVal;
@@ -30,11 +45,36 @@ const [search, setSearch] = useState("");
         console.log("productId = "+productId);
         console.log(cart);
 //      console.log("prodId = "+prodId);
+      //const imgurl =`http://peaciotest.com:3000/images/${data.data[0].partImgUrl}`;
+      const imgurl =`/images/${data.data[0].partImgUrl}`;
+console.log(imgurl);
         return (
           <>
 	  <div>
-          <h1>Product {productId} </h1>
+          <h1>{data.data[0].partShortDesc} </h1>
           </div>
+		<div>
+                    <Container>
+                       <Row>
+                           <Col><img src={imgurl} className="img-fluid shadow-4" 
+			   alt={data.data[0].partDesc} /></Col>
+                           <Col>
+		               <Row><Col>{data.data[0].partDesc}</Col></Row>
+                          <Row> <Col>Part: {data.data[0].partNumber}</Col></Row>
+                          <Row> <Col>Brand Part: {data.data[0].manPartNumber}</Col></Row>
+                          <Row> <Col>Part Option: {data.data[0].partOption}</Col></Row>
+                          <Row> <Col>Merchant: {data.data[0].merchantName}</Col></Row>
+                          <Row> <Col>Price: $ {data.data[0].partSalePrice.toFixed(2)}</Col></Row>
+
+                   	<Row> <Col>	<Button sm="6" onClick={()=>cart.addOneToCart(data.data[0])  } 
+		className="mx-2">Add to Cart</Button></Col></Row>
+
+		           </Col> 
+		       </Row>
+<hr />
+		    </Container>
+
+		</div>
 		</>
   )
 }
