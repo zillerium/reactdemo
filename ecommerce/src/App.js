2a3453e7454d184ffdb88666d68d4d28b5d8a1a5
sales@merrows.co.uk <sales@merrows.co.uk>
2023-01-09 16:39:39 +0000

    ecommerce

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -11,6 +11,7 @@ import {Link, BrowserRouter ,  Routes, Route} from 'react-router-dom';
 import Cancel from './pages/Cancel'
 import Store from './pages/Store'
 import ProductPage from './pages/ProductPage'
+import AddProduct from './pages/AddProduct'
 import Success from './pages/Success'
 import CartProvider from './CartContext.js'
 
@@ -106,167 +107,13 @@ return ( data ? <img src={data.data.url}/> : <p></p>);
                      <Route path="succcess" element={<Success />} />                    
                      <Route path="cancel" element={<Cancel />} />     
                      <Route path="/product/:productId" element={<ProductPage />} />
+                     <Route path="/addproduct/" element={<AddProduct />} />
 	        </Routes>
           <div>
 	  </div>
 	  </Container>
 </CartProvider>
 
-      <header >
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <div>
-	  </div>
-	  <div>
-  <Button />
-	  <button onClick={refetch}>Get </button>
-         </div>
-	  Enter Part Data - partTechImgUrlmanPartNumber
-	  <div>
-            <label>Man Name</label>
-	    <input type="text" 
-	       placeholder="Man Name  " 
-	       value={manName} 
-	       onChange={(e)=>setManName(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Number</label>
-	    <input type="text" 
-	       placeholder="Part Number  " 
-	       value={partNumber} 
-	       onChange={(e)=>setPartNumber(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Man Part Number</label>
-	    <input type="text" 
-	       placeholder="Man Part Number " 
-	       value={manPartNumber} 
-	       onChange={(e)=>setManPartNumber(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Man Part Option</label>
-	    <input type="text" 
-	       placeholder="Part Option " 
-	       value={partOption} 
-	       onChange={(e)=>setPartOption(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Desc</label>
-	    <input type="text" 
-	       placeholder="Part Desc " 
-	       value={partDesc} 
-	       onChange={(e)=>setPartDesc(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Short Desc</label>
-	    <input type="text" 
-	       placeholder="Part Short Desc " 
-	       value={partShortDesc} 
-	       onChange={(e)=>setPartShortDesc(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Img Url</label>
-	    <input type="text" 
-	       placeholder="Part Img Url " 
-	       value={partImgUrl} 
-	       onChange={(e)=>setPartImgUrl(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Tech Img Url</label>
-	    <input type="text" 
-	       placeholder="Par Tech Img Url " 
-	       value={partTechImgUrl} 
-	       onChange={(e)=>setPartTechImgUrl(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Sale Price</label>
-	    <input type="text" 
-	       placeholder="Part SalePrice " 
-	       value={partSalePrice} 
-	       onChange={(e)=>setPartSalePrice(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Part Man Price</label>
-	    <input type="text" 
-	       placeholder="Part ManPrice " 
-	       value={partManPrice} 
-	       onChange={(e)=>setPartManPrice(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Currency</label>
-	    <input type="text" 
-	       placeholder="Currency " 
-	       value={currency} 
-	       onChange={(e)=>setCurrency(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Merchant ID</label>
-	    <input type="text" 
-	       placeholder="Merchant Id " 
-	       value={merchantId} 
-	       onChange={(e)=>setMerchantId(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Merchant Name</label>
-	    <input type="text" 
-	       placeholder="Merchant Name " 
-	       value={merchantName} 
-	       onChange={(e)=>setMerchantName(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-            <label>Delivery Charge</label>
-	    <input type="text" 
-	       placeholder="Delivery Charge " 
-	       value={deliveryCharge} 
-	       onChange={(e)=>setDeliveryCharge(e.target.value)} 
-	    />
-	  </div>
-	  <div>
-           <button onClick={()=>mutate({
-		   manName:manName, 
-		   partNumber: partNumber,
-		   manPartNumber: manPartNumber,
-		   partDesc: partDesc,
-		   partOption: partOption,
-		   partShortDesc: partShortDesc,
-		   partImgUrl: partImgUrl,
-		   partTechImgUrl: partTechImgUrl,
-		   partSalePrice: partSalePrice,
-		   partManPrice: partManPrice,
-		   currency: currency,
-		   merchantId: merchantId,
-		   merchantName: merchantName,
-		   deliveryCharge: deliveryCharge }
-
-	   )}>Add Part</button>
-	  </div>
-	  <div>
-            <label>First Name </label>{firstName}
-	    <input type="text" placeholder="First " value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
-	  </div>
-	  <div>
-            <label>Last Name </label>
-	    <input type="text" placeholder="Last " value={lastName} onChange={(e)=>setLastName(e.target.value)} />
-	  </div>
-	  <div>
-           <button onClick={()=>mutate({firstName:firstName, lastName, lastName})}>Post</button>
-	  </div>
-	  <ImageDisplay />
-      </header>
     </div>
   );
 }
